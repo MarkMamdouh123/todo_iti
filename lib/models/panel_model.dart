3df9e06c51104fd0217160dc06cf65c5cfdd9cc0
Mark Mamdouh <mark.mamdouh67@yahoo.com>
2024-09-19 22:16:35 +0300

    imported hive and created type adapter for panel_model

--- lib/models/panel_model.dart
+++ lib/models/panel_model.dart
@@ -1,7 +1,15 @@
-class PanelModel {
+import 'package:hive/hive.dart';
+part 'panel_model.g.dart';
+
+@HiveType(typeId: 0)
+class PanelModel extends HiveObject {
+  @HiveField(0)
   List<TaskItem> items;
+  @HiveField(1)
   String time;
+  @HiveField(2)
   bool? isCompleted;
+  @HiveField(3)
   bool isExpanded;
 
   PanelModel({
@@ -12,8 +20,11 @@ class PanelModel {
   });
 }
 
+@HiveType(typeId: 1)
 class TaskItem {
+  @HiveField(0)
   String description;
+  @HiveField(1)
   bool isDone;
 
   TaskItem({required this.description, required this.isDone});
